4934abb80ec7c8e0232ceb3f730efda7fe26b8fb
GHT <guglielmohenriquestargino@gmail.com>
2026-05-22 11:23:59 -0300

    pgte_service
    Aplicação do pacote service

--- api/src/main/java/com/dto/api/controller/Control.java
+++ api/src/main/java/com/dto/api/controller/Control.java
@@ -1,23 +1,37 @@
 package com.dto.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dto.api.service.Operacao;
+
 @RestController
 @RequestMapping("/lojinha")
 public class Control {
 
+  private String item;
+  @Autowired
+  private Operacao resu;
 
-    @GetMapping("/galeria")
-    public ResponseEntity<String> teste(){
 
-        return new ResponseEntity<>("Lojinha Legal!", HttpStatus.OK);
-        
+ @GetMapping("/credito")
+    public ResponseEntity<String> testar(){
+      item = resu.creditar();
+
+        return new ResponseEntity<String>(item, HttpStatus.OK); 
     }
 
+  @GetMapping("/debito")
+    public ResponseEntity<String> testa2(){
+      item = resu.debitar();
 
+        return new ResponseEntity<String>(item, HttpStatus.OK); 
+    }
+ 
 
+    ////////////fim control
 }
---
+++ api/src/main/java/com/dto/api/service/Operacao.java
@@ -0,0 +1,23 @@
+package com.dto.api.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Operacao {
+
+    private String item=null;
+
+    public String creditar(){
+
+        item = "Creditou";
+        return item;
+    }
+
+    public String debitar(){
+
+        item = "Debitou";
+        return item;
+    }
+
+
+}
